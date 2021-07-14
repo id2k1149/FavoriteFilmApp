88b61abb911c882a3cb060a1174033047bf9a42d
id2k1149 <id2k1149@gmail.com>
2021-07-14 14:08:44 -0400

    translate to English

--- filmproject/polls/views.py
+++ filmproject/polls/views.py
@@ -1,10 +1,8 @@
-from time import strptime
-
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
-from .models import Question, Answer
+from .models import Question
 import datetime
 
 
@@ -14,11 +12,11 @@ class IndexView(ListView):
     context_object_name = 'we_have_questions'
 
     def get_queryset(self):
-        # """Return sorted questions"""
-        # questions = Question.objects.order_by('-date_published')
         """Return filtered questions"""
-        if datetime.datetime.now().hour < 11:
-            questions = Question.objects.filter(date_published__date=datetime.date.today())
+        if datetime.datetime.now().hour < 23:
+            questions = Question.objects\
+                .filter(date_published__date=datetime.date.today())\
+                .order_by('-date_published')
             return questions
 
 
@@ -30,7 +28,7 @@ class QuestionDetailView(DetailView):
 def vote(request, poll_id):
     question = get_object_or_404(Question, pk=poll_id)
     if not question.is_active:
-        return HttpResponse('Опрос снят с публикации')
+        return HttpResponse('Sorry, this question is not actual now')
     else:
         if request.POST.get('answer'):
             try:
@@ -40,27 +38,18 @@ def vote(request, poll_id):
                     ValueError):
                 return render(request, 'polls/detail.html', {
                     'question': question,
-                    'error_message': "Указан недопустимый ответ",
+                    'error_message': "Invalid answer",
                 })
-
             selected_answer.votes += 1
             selected_answer.save()
             return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
         else:
             return render(request, 'polls/detail.html', {
                 'question': question,
-                'error_message': "Вы не выбрали ответ.",
+                'error_message': "Please, choose an answer",
             })
 
 
 class ResultsView(DetailView):
     model = Question
     template_name = 'polls/results.html'
-
-    def get_queryset(self):
-        results = Question.objects.all()
-        return results
-
-
-def result(request, poll_id):
-    return HttpResponseRedirect(reverse('polls:results'))
